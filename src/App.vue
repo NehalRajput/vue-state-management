8ef88e1dc79c5d290d4a6231ae161ad4e87ddcd5
NehalRajput <nehal17.ast@gmail.com>
2025-04-14 11:22:55 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+
+<script setup>
+import CounterComponent from './components/counter.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Welcome</h1>
+    <CounterComponent />
+  </div>
+</template>
---
+++ src/components/counter.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
+</script>
+
+<template>
+  <h2>Count: {{ counter.count }}</h2>
+  <button @click="counter.increment">Increment</button>
+</template>
